78dadb99579e3855fc7b8bc72b3d478ff7d8bc26
José Miguel <33165751+josemird@users.noreply.github.com>
2025-04-11 22:17:33 +0200

    home changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user/user.service';
-import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   private userService = inject(UserService);
-  private authService = inject(AuthService);
   users: any[] = [];
-  loggedUser: any = {};
   loading = true;
 
   ngOnInit() {
-    this.loggedUser = this.authService.getLoggedUser();
-
     this.userService.getUsers().subscribe({
       next: (res) => {
-        this.users = res.users.filter((user: any) => user.id !== this.loggedUser.id);
+        this.users = res.users;
         this.loading = false;
       },
       error: () => {
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,28 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: 'root'})
 export class AuthService {
   private apiUrl = 'https://vps-ff89e3e0.vps.ovh.net/api';
   private tokenKey = 'token';
-  private userKey = 'user';
   private isAuthenticated$ = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) {
-
     const token = localStorage.getItem(this.tokenKey);
-    const user = localStorage.getItem(this.userKey);
-
-    if (token && user) {
-      this.isAuthenticated$.next(true);
-    }
+    this.isAuthenticated$.next(!!token);
   }
 
   login(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
       tap((res: any) => {
-        localStorage.setItem(this.tokenKey, res.access_token);
-        localStorage.setItem(this.userKey, JSON.stringify(res.user));
+        localStorage.setItem(this.tokenKey, res.token);
         this.isAuthenticated$.next(true);
       })
     );
@@ -35,20 +28,11 @@ export class AuthService {
 
   logout(): void {
     localStorage.removeItem(this.tokenKey);
-    localStorage.removeItem(this.userKey);
     this.isAuthenticated$.next(false);
   }
 
   getToken(): string | null {
     return localStorage.getItem(this.tokenKey);
-  }
-
-  getLoggedUser(): any {
-    const user = localStorage.getItem(this.userKey);
-    if (user) {
-      return JSON.parse(user);
-    }
-    return null;
   }
 
   isLoggedIn(): Observable<boolean> {
