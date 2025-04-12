22e12f48a6b206b162854ad591911793ea1c3d44
José Miguel <33165751+josemird@users.noreply.github.com>
2025-04-12 10:54:31 +0200

    auth review

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
       tap((res: any) => {
         localStorage.setItem(this.tokenKey, res.token);
         localStorage.setItem('user', JSON.stringify(res.user)); //* revisar este guardado
-        console.log(res.user);
+        console.log('user');
         this.isAuthenticated$.next(true);
       })
     );
