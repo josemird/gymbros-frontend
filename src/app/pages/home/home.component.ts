9cfe5020c48cc5b061dba86b14281276fbd4e91c
José Miguel <josemidaw22@gmail.com>
2025-04-14 14:10:03 +0200

    profile user tab

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user/user.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +17,13 @@ export class HomeComponent implements OnInit {
   loading = true;
 
   ngOnInit() {
-    this.userService.getUsers().subscribe({
-      next: (res) => {
-        const currentUser = this.userService.getCurrentUser();
-        console.log(currentUser);
-        this.users = res.users.filter((user: any) => user.email !== currentUser?.email);
-        this.loading = false;
+    combineLatest([
+      this.userService.watchCurrentUser$(),
+      this.userService.getUsers()
+    ]).subscribe({
+        next:([currentUser, res]) => {
+          this.users = res.users.filter((user: any) => user.email !== currentUser?.email);
+          this.loading = false;
       },
       error: () => {
         this.loading = false;
