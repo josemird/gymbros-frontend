39ca55a2a3aee2b62d49f1c6acfbf11540448f6e
José Miguel <josemidaw22@gmail.com>
2025-04-22 16:56:52 +0200

    chat changes

--- src/app/pages/messages/chat/chat.component.ts
+++ src/app/pages/messages/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from '../../../services/message/message.service';
 import { UserService } from '../../../services/user/user.service';
 import { FormsModule } from '@angular/forms';
@@ -16,16 +16,27 @@ export class ChatComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private messageService = inject(MessageService);
   private userService = inject(UserService);
+  router = inject(Router);
 
   currentUser: any;
   receiverId!: number;
+  receiverUser: any = null;
   messages: any[] = [];
   newMessage: string = '';
   loading = true;
 
+  backendUrl = 'https://vps-ff89e3e0.vps.ovh.net/uploads/';
+  defaultAvatar = 'https://pentaxcenter.com/wp-content/uploads/no-user-image-square.jpg';
+
   ngOnInit() {
     this.receiverId = Number(this.route.snapshot.paramMap.get('id'));
     this.currentUser = this.userService.getCurrentUser();
+
+    this.userService.getUsers().subscribe({
+      next: (res) => {
+        this.receiverUser = res.users.find((u: any) => u.id === this.receiverId);
+      }
+    });
 
     this.messageService.getMessages().subscribe({
       next: (res) => {
@@ -35,7 +46,6 @@ export class ChatComponent implements OnInit {
             (msg.sender_id === this.receiverId && msg.receiver_id === this.currentUser.id)
           );
         });
-
         this.loading = false;
       },
       error: () => {
