2e14c7795083f2f8c3f5b84d8f4240193cbcfaba
José Miguel <josemidaw22@gmail.com>
2025-04-24 18:45:03 +0200

    chat changes

--- src/app/pages/messages/chat/chat.component.ts
+++ src/app/pages/messages/chat/chat.component.ts
@@ -50,6 +50,7 @@ export class ChatComponent implements OnInit {
         });
         this.loading = false;
         this.scrollToBottom();
+        this.markMessagesAsRead();
       },
       error: () => {
         this.loading = false;
@@ -77,5 +78,15 @@ export class ChatComponent implements OnInit {
     });
   }
 
+  markMessagesAsRead() {
+    const unreadMessages = this.messages.filter(
+      msg => msg.receiver_id === this.currentUser.id && !msg.read
+    );
+
+    unreadMessages.forEach(msg => {
+      this.messageService.markAsRead(msg.id).subscribe();
+      msg.read = true;
+    });
+  }
 
 }
