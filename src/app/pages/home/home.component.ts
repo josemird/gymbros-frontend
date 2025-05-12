21e0dc8ec7d818dd819a58c6078596f97c037851
José Miguel <josemidaw22@gmail.com>
2025-05-12 12:13:45 +0200

    home: gym filter

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,37 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { UserService } from '../../services/user/user.service';
 import { LikeService } from '../../services/like/like.service';
+import { GymService } from '../../services/gym/gym.service';
 import { UserCardComponent } from '../../shared/user-card/user-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, UserCardComponent],
+  imports: [CommonModule, UserCardComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   private userService = inject(UserService);
   private likeService = inject(LikeService);
+  private gymService = inject(GymService);
 
   users: any[] = [];
+  filteredUsers: any[] = [];
+  gyms: any[] = [];
+  selectedGymId: number | null = null;
   loading = true;
 
   ngOnInit() {
     this.loading = true;
+
+      this.gymService.getGyms().subscribe({
+      next: (res) => {
+        this.gyms = res.gyms;
+      }
+    });
 
     this.userService.watchCurrentUser$().subscribe(currentUser => {
       if (!currentUser) return;
@@ -35,6 +46,7 @@ export class HomeComponent implements OnInit {
               ...user,
               liked: likedUserIds.includes(user.id)
             }));
+            this.applyFilter();
             this.loading = false;
           });
         },
@@ -43,6 +55,18 @@ export class HomeComponent implements OnInit {
         }
       });
     });
+  }
+
+    applyFilter() {
+    if (this.selectedGymId) {
+      this.filteredUsers = this.users.filter(u => u.gym?.id === this.selectedGymId);
+    } else {
+      this.filteredUsers = [...this.users];
+    }
+  }
+
+  onGymChange() {
+    this.applyFilter();
   }
 
   toggleLike(user: any) {
